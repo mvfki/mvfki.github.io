ab1b971bace451c151c6855c01371ee99c1e7ccc
Yichen Wang <wangych@bu.edu>
2020-06-22 00:52:59 -0400

    css center img/iframe test

--- js/init.js
+++ js/init.js
@@ -13,7 +13,8 @@
 			// Global.
 				global: {
 					range: '*',
-					href: 'css/style.css',
+					//href: 'css/style.css',
+					href: 'https://mvfki.github.io/css/style.css',
 					containers: 1400,
 					grid: {
 						gutters: {
@@ -26,14 +27,16 @@
 			// XLarge.
 				xlarge: {
 					range: '-1680',
-					href: 'css/style-xlarge.css',
+					//href: 'css/style-xlarge.css',
+					href: 'https://mvfki.github.io/css/style-xlarge.css',
 					containers: 1200
 				},
 
 			// Large.
 				large: {
 					range: '-1280',
-					href: 'css/style-large.css',
+					//href: 'css/style-large.css',
+					href: 'https://mvfki.github.io/css/style-large.css',
 					containers: 960,
 					grid: {
 						gutters: {
@@ -48,7 +51,8 @@
 			// Medium.
 				medium: {
 					range: '-980',
-					href: 'css/style-medium.css',
+					//href: 'css/style-medium.css',
+					href: 'https://mvfki.github.io/css/style-medium.css',
 					containers: '90%',
 					grid: {
 						collapse: 1
@@ -58,7 +62,8 @@
 			// Small.
 				small: {
 					range: '-736',
-					href: 'css/style-small.css',
+					//href: 'css/style-small.css',
+					href: 'https://mvfki.github.io/css/style-small.css',
 					containers: '90%',
 					grid: {
 						gutters: {
@@ -70,7 +75,8 @@
 			// XSmall.
 				xsmall: {
 					range: '-480',
-					href: 'css/style-xsmall.css',
+					//href: 'css/style-xsmall.css',
+					href: 'https://mvfki.github.io/css/style-xsmall.css',
 					grid: {
 						collapse: 2
 					}
