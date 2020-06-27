5f6788d257063f1f37cf6a239f10c14f8fffd988
Yichen Wang <wangych@bu.edu>
2020-06-27 04:24:26 -0400

    try batch render instead of stating each document in HTML

--- js/posts.js
+++ js/posts.js
@@ -1,50 +1,161 @@
-function loadArticle(name, divID) {
+function loadArticle(topic) {
+    //document.getElementById("articleDiv").innerHTML = "<input type='file' id='readContIn' onchange='loadFromContentFile(\"" + topic + "\");'/>";
     if(window.location.href.startsWith('file://')){
-        document.getElementById(divID).innerHTML = "<input type='file' id='in" + divID + "' onchange='readLocal(\"in" + divID + "\", \""+ divID + "\")'/>";
+        document.getElementById("articleDiv").innerHTML = "<input type='file' id='readContIn' onchange='loadInputContents(\"" + topic + "\");' multiple>All MD to load</input>";
     } else if(window.location.href.startsWith('https://')){
-        var text = readOnline(name, divID);
+        loadFromContentFile(topic);
     }
 };
 
-function readLocal(inid, divID) {
+// Local loader functions
+function insertFile(file, divID){
+    var reader = new FileReader();
     var converter = new showdown.Converter();
-    var files = document.getElementById(inid).files;
-    var file = files[0];
-    const reader = new FileReader();
-    reader.onloadend = function(evt) {
+    reader.onload = function(evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
             var text = evt.target.result;
             var html = converter.makeHtml(text);
             document.getElementById(divID).innerHTML = html;
-        };
+            hideArticle(divID);
+            var title = document.getElementById(divID).getElementsByTagName('*')[0];
+            title.onclick = function(){
+                showArticle(divID);
+            }
+        }
     }
-    console.log(file)
     var blob = file.slice(0, file.size);
     reader.readAsText(blob);
-};
+}
 
-function readOnline(filename, divID) {
+function setHider(hider, divID) {
+    hider.onclick = function(){
+        hideArticle(divID);
+    }
+}
+
+function loadInputContents(topicAbbr){
+    var files = document.getElementById("readContIn").files;
+    var artDiv = document.getElementById("articleDiv");
+    for (let i = 0; i < files.length; i++) {
+        var file = files[i];
+        // Create section
+        var sec = document.createElement("section");
+        var month = file.name.substring(0,7);
+        sec.className = "article " + month;
+        // Create div
+        var postDiv = document.createElement("div");
+        postDiv.className = "image post";
+        postDiv.id = topicAbbr + (i+1).toString();
+        // Read MD file and convert to HTML
+        insertFile(file, postDiv.id);
+        
+        var hider = document.createElement("a");
+        setHider(hider, postDiv.id);
+        hider.id = "hide" + postDiv.id;
+        var hiderText = document.createTextNode("Hide");
+        hider.appendChild(hiderText);
+
+        var hr = document.createElement("hr");
+
+        sec.appendChild(postDiv);
+        sec.appendChild(hider);
+        sec.appendChild(hr);
+        artDiv.appendChild(sec);
+    }
+    
+}
+
+// Online loader functions
+function insertOnlineMD(filename, divID) {
     var converter = new showdown.Converter();
     var request = new XMLHttpRequest();
-    //var url = 'https://raw.githubusercontent.com/mvfki/mvfki.github.io/master/' + name;
-    request.open('GET', filename, true);
+    //var url = 'https://raw.githubusercontent.com/mvfki/mvfki.github.io/master/blog/coding/posts/' + filename;
+    var url = 'posts/' + filename;
+    request.open('GET', url, true);
     request.send(null);
     request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 200) {
-        var type = request.getResponseHeader('Content-Type');
-        if (type.indexOf("text") !== 1) {
-            var text = request.responseText;
-            var html = converter.makeHtml(text);
-            document.getElementById(divID).innerHTML = html;
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                var text = request.responseText;
+                var html = converter.makeHtml(text);
+                document.getElementById(divID).innerHTML = html;
+                hideArticle(divID);
+                var title = document.getElementById(divID).getElementsByTagName('*')[0];
+                title.onclick = function(){
+                    showArticle(divID);
+                }
+            } else {
+                document.getElementById(divID).innerHTML = "Not text file";
+            }
         } else {
-            document.getElementById(divID).innerHTML = "Not text file";
+            document.getElementById(divID).innerHTML = "Request state error";
         }
-    } else {
-        document.getElementById(divID).innerHTML = "Request state error";
     }
-    }
-    };
+}
 
+function loadMDs(mds, topicAbbr) {
+    var artDiv = document.getElementById("articleDiv");
+    for(var i = 0; i < mds.length; i++) {
+        var sec = document.createElement("section");
+        var month = mds[i].substring(0,7);
+        sec.className = "article " + month;
+        // Create div
+        var postDiv = document.createElement("div");
+        postDiv.className = "image post";
+        postDiv.id = topicAbbr + (i+1).toString();
+        // Read MD file and convert to HTML
+        insertOnlineMD(mds[i], postDiv.id);
+        var hider = document.createElement("a");
+        setHider(hider, postDiv.id);
+        hider.id = "hide" + postDiv.id;
+        var hiderText = document.createTextNode("Hide");
+        hider.appendChild(hiderText);
+
+        var hr = document.createElement("hr");
+
+        sec.appendChild(postDiv);
+        sec.appendChild(hider);
+        sec.appendChild(hr);
+        artDiv.appendChild(sec);
+    }
+}
+
+function loadFromContentFile(topic) {
+    // For Local Debug Use
+    // var files = document.getElementById("readContIn").files;
+    // var contentFile = files[0];
+    // var reader = new FileReader();
+    // reader.onload = function(evt) {
+    //     if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+    //         var text = evt.target.result;
+    //         var mds = text.split("\n");
+    //         loadMDs(mds, topic)
+    //     }
+    // }
+    // var blob = contentFile.slice(0, contentFile.size);
+    // reader.readAsText(blob);
+    var request = new XMLHttpRequest();
+    var url = 'posts/CONTENTS.txt';
+    request.open('GET', url, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                var text = request.responseText;
+                var mds = text.split('\n');
+                loadMDs(mds, topic);
+            } else {
+                document.getElementById("articleDiv").innerHTML = "Loading Error";
+            }
+        } else {
+            document.getElementById("articleDiv").innerHTML = "Loading Error";
+        }
+    }
+};
+
+// Dynamic effect functions
 function showMonth() {
     var sel = document.getElementById("selMonth").value;
     var allArt = document.getElementsByClassName("article");
@@ -64,3 +175,29 @@ function showMonth() {
         }
     }
 }
+
+function hideArticle(id) {
+    var contents = document.getElementById(id).getElementsByTagName("*");
+    for (var i = 1; i < contents.length; i++) {            
+        contents[i].style.display = 'none';
+    }
+    document.getElementById("hide"+id).style.display = 'none';
+}
+
+function showArticle(id) {
+    var contents = document.getElementById(id).getElementsByTagName("*");
+    for (var i = 1; i < contents.length; i++) {
+        if (contents[i].tagName == "CODE"){
+            if(contents[i-1].tagName != "PRE"){
+                contents[i].style.display = "unset";
+            } else {
+                contents[i].style.display = 'block';
+            }
+        } else {
+            contents[i].style.display = 'block';
+        }
+    }
+    document.getElementById("hide"+id).style.display = 'block';
+}
+
+
